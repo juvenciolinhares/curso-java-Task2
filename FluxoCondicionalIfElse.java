775766f686eab6a7f68f1f8f0832debf3d752c97
Juvencio <jlinhares@cpqi.com>
2022-06-09 15:46:30 -0300

    Fluxo Condicional If Else

---
+++ FluxoCondicionalIfElse.java
@@ -0,0 +1,32 @@
+
+public class FluxoCondicionalIfElse {
+
+	public static void main(String[] args) {
+
+		int idade = 31;
+		if(idade <=10) {
+			System.out.println("Crianca");
+		} else if(idade >= 11 && idade <= 18) {
+			System.out.println("Adolescente");
+		} else if(idade > 18 && idade <=60) {
+			System.out.println("Adulto");
+		} else  {
+			System.out.println("Melhor Idade");
+		}
+		
+		//if aninhado 
+		
+		int nota = 6;
+		if(nota >= 7) {
+			System.out.println("Passou");
+		} else {
+			System.out.println("reprovou");
+			if (nota >= 6) {
+				System.out.println("Mas pode  fazer a recuperação");
+			}
+			
+		}
+
+	}
+
+}
