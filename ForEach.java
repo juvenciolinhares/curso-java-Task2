2651a48358920340615d18e067c04f0c8e9eec3f
Juvencio <jlinhares@cpqi.com>
2022-06-11 12:07:48 -0300

    Laco For Each

---
+++ ForEach.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class ForEach {
+
+	public static void main(String[] args) {
+		int[] pares = {2,4,6,8};
+		
+		//laco for comum:
+		for(int i = 0; i < pares.length; i++) {
+			int par = pares[i];
+			System.out.println(par);
+		}
+		
+		//for each:
+		for( int par : pares) {
+			System.out.println(par);
+		}
+		/*limitação:
+		 * -não tem acesso ao int = i;
+		 */
+		
+		/*outro exemplo:
+		 * preencher um ArrayList usando o for
+		 * apresentando usado for each:
+		 */
+		ArrayList<Integer> list = new ArrayList();
+		
+		for(int i = 0; i < 10; i ++) {
+			list.add(i);
+		}
+		
+		for (Integer numero : list) {
+			System.out.println(numero);
+		}
+		
+	}
+
+}
