2e9d2a9c044bf1f3e36f4847d737ca5e8f2e7cfc
Juvencio <jlinhares@cpqi.com>
2022-06-11 12:06:59 -0300

    Laco For

---
+++ For.java
@@ -0,0 +1,14 @@
+
+public class For {
+
+	public static void main(String[] args) {
+		
+		String texto = " ";
+		for(int i = 10; i > 0; i --) {
+			texto += i + ", ";
+		}
+		System.out.println(texto);
+
+	}
+
+}
